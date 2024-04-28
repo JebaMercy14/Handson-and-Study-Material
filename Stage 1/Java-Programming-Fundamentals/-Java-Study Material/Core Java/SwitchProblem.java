f149d6f6d9abe63dcc7de2b72927240cd265e027
Kothai_S <102270819+JebaMercy14@users.noreply.github.com>
2024-04-28 14:40:37 +0530

    Add files via upload

---
+++ Stage 1/Java-Programming-Fundamentals/-Java-Study Material/Core Java/SwitchProblem.java
@@ -0,0 +1,18 @@
+public class SwitchProblem {
+
+public static void main(String[] args) {
+	switch(input)
+	{
+	case2: 
+		System.out.println("2");
+	int input = 3;
+	break;
+	case1:
+		System.out.println("1");
+	break;
+	default:
+		System.out.println("3");
+}
+
+}
+}
